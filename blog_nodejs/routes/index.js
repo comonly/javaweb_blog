73d8946f1ed30435a5a077d93ed6cdae2f0e7d55
chengchen.local <1982989993@qq.com>
2019-11-14 23:36:02 +0800

    新增request库用来请求服务器json数据，添加获取文章列表和详情

--- blog_nodejs/routes/index.js
+++ blog_nodejs/routes/index.js
@@ -1,25 +1,33 @@
 var express = require('express');
+var request= require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-//数据
-    var data = {
-    title: '国内要闻',
-      time: (new Date).toString(),
-      list: [
-          {
-            id: '1',
-                name: '张三'
-          },
-          {
-                id: '2',
-                name: '李四'
-          }
-      ]
-    };
-    //渲染模板
-    res.render('index', data);
+/*正式数据*/
+  request('http://localhost:8080/blog_spring_backend/passage/passIndex',function(error,response,body){
+    /*判断请求是否成功*/
+    if (!error && response.statusCode == 200) {
+      /*把字符串转换为json*/
+      var data=JSON.parse('{"list":'+body+'}');
+      /*渲染模板*/
+      res.render('index', data);
+    }
+  });
 });
+
+//查看文章详情
+router.get('/detail', function(req, res, next) {
+  /*正式数据*/
+    request('http://localhost:8080/blog_spring_backend/passage/passDetail?id='+req.param('id'),function(error,response,body){
+      /*判断请求是否成功*/
+      if (!error && response.statusCode == 200) {
+        /*把字符串转换为json*/
+        var data=JSON.parse(body);
+        /*渲染模板*/
+        res.render('detail', data);
+      }
+    });
+  });
 
 module.exports = router;
